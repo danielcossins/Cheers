3221e52f9ec2caae24d3f46ae42c6abf4d52b914
danielcossins11 <daniel.cossins@yahoo.com>
2015-10-13 15:22:31 -0500

    figured out halfnorsemix

---
+++ Cheers/Cheers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cheers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello there! What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            for(int i=0; i<name.Length; i++)
+            {
+                /*This checks for upper and lower case*/
+                bool isVowel = "halfnorsemix".IndexOf(name[i].ToString(), StringComparison.InvariantCultureIgnoreCase) >= 0;
+                if (isVowel){
+                    Console.WriteLine("Give me an "+Char.ToLower(name[i])+" ... " + name[i]);
+                }
+                else
+                {
+                    Console.WriteLine("Give me a "+Char.ToLower(name[i])+" ... " + name[i]);
+                }
+            }
+            Console.WriteLine("Gooooooooooo " + name + "!");
+        }
+    }
+}
