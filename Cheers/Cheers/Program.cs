961e42995310ddb5dae245b9b2e78079b31f6b4f
danielcossins11 <daniel.cossins@yahoo.com>
2015-10-15 12:14:51 -0500

    Added birthday calculation.

--- Cheers/Cheers/Program.cs
+++ Cheers/Cheers/Program.cs
@@ -9,8 +9,9 @@ namespace Cheers
             
             Console.WriteLine("Hello there! What is your name?");
             string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
-            for(int i=0; i<name.Length; i++)
+            Console.WriteLine("Hey " + name + ", what's your birthday? (MM/DD)");
+            string birthday = Console.ReadLine();
+            for (int i=0; i<name.Length; i++)
             {
                 /*This checks for upper and lower case*/
                 bool isVowel = "halfnorsemix".IndexOf(name[i].ToString(), StringComparison.InvariantCultureIgnoreCase) >= 0;
@@ -23,6 +24,28 @@ namespace Cheers
                 }
             }
             Console.WriteLine("Gooooooooooo " + name + "!");
+
+            //Birthday----------------------------
+            DateTime today = DateTime.Now;
+            DateTime convertedBD = DateTime.Parse(birthday);
+
+            int difference = (convertedBD-today).Days;
+            //difference++;
+            if (difference < 0)
+            {
+                difference = 365 + difference;
+            }
+            if(difference == 0)
+            {
+                Console.WriteLine("It's your birthday!");
+            }
+            else
+            {
+                Console.WriteLine(difference + " days till your birthday");
+            }
+
+            Console.WriteLine("Press any key to finish . . .");
+            Console.ReadKey();
         }
     }
 }
