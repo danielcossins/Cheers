519b5d151e68fb0c77451ebcd9773e2611c45b83
danielcossins11 <daniel.cossins@yahoo.com>
2015-10-15 14:07:55 -0500

    Added a function for try/catch

--- Cheers/Cheers/Program.cs
+++ Cheers/Cheers/Program.cs
@@ -6,27 +6,36 @@ namespace Cheers
     {
         static void Main(string[] args)
         {
-            
+
             Console.WriteLine("Hello there! What is your name?");
             string name = Console.ReadLine();
-            Console.WriteLine("Hey " + name + ", what's your birthday? (MM/DD)");
-            string birthday = Console.ReadLine();
-            for (int i=0; i<name.Length; i++)
+            for (int i = 0; i < name.Length; i++)
             {
                 /*This checks for upper and lower case*/
                 bool isVowel = "halfnorsemix".IndexOf(name[i].ToString(), StringComparison.InvariantCultureIgnoreCase) >= 0;
-                if (isVowel){
-                    Console.WriteLine("Give me an "+Char.ToLower(name[i])+" ... " + name[i]);
+                if (isVowel)
+                {
+                    Console.WriteLine("Give me an " + Char.ToLower(name[i]) + " ... " + name[i]);
                 }
                 else
                 {
-                    Console.WriteLine("Give me a "+Char.ToLower(name[i])+" ... " + name[i]);
+                    Console.WriteLine("Give me a " + Char.ToLower(name[i]) + " ... " + name[i]);
                 }
             }
             Console.WriteLine("Gooooooooooo " + name + "!");
 
             //Birthday----------------------------
+            Console.WriteLine("Hey " + name + ", what's your birthday? (MM/DD)");
+            string birthday = Console.ReadLine();
             DateTime today = DateTime.Now;
+            findBD(birthday, today);
+
+            Console.WriteLine("Press any key to finish . . .");
+            Console.ReadKey();
+        }
+
+        private static void findBD(string birthday, DateTime today)
+        {
             try
             {
                 DateTime convertedBD = DateTime.Parse(birthday);
@@ -58,16 +67,13 @@ namespace Cheers
                 {
                     Console.WriteLine(difference + " days till your birthday");
                 }
+                return;
             }
-            catch
+            catch(FormatException)
             {
                 Console.WriteLine("not a valid birthday");
+                findBD(Console.ReadLine(), DateTime.Today);
             }
-
-            
-
-            Console.WriteLine("Press any key to finish . . .");
-            Console.ReadKey();
         }
     }
 }
